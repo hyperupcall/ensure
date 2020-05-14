9261c5940a0597e4480468e53eede307e69f0e54
eankeen <24364012+eankeen@users.noreply.github.com>
2020-05-13 18:40:48 -0700

    feat: add script exit if criteria not met

--- mod.ts
+++ mod.ts
@@ -1,7 +1,4 @@
-import * as path from "./vendor/https/deno.land/std/fs/path.ts";
-import { isOutOfDateVersion } from "./src/compare.ts";
-
-console.log(path.join(Deno.cwd(), "example"));
+import { isOutdated } from "./src/compare.ts";
 
 interface pleaseUpgradeDenoOptions {
   denoVersion: string;
@@ -12,5 +9,36 @@ interface pleaseUpgradeDenoOptions {
 export function pleaseUpgradeDeno(
   pleaseUpgradeDenoOptions: pleaseUpgradeDenoOptions,
 ) {
-  const { deno, v8, typescript } = Deno.version;
+  const { deno: currentDeno, v8: currentV8, typescript: currentTypescript } =
+    Deno.version;
+
+  const {
+    denoVersion: expectedDeno,
+    v8Version: expectedV8,
+    typescriptVersion: expectedTypescript,
+  } = pleaseUpgradeDenoOptions;
+
+  const isDenoOutdated = isOutdated(currentDeno, expectedDeno);
+  const isV8Outdated = isOutdated(currentV8, expectedV8);
+  const isTypescriptOutdated = isOutdated(
+    currentTypescript,
+    expectedTypescript,
+  );
+
+  const msg = (type: string, current: string, expected: string) =>
+    `Your ${type} version is ${current}, but at least version ${expected} is required. Please update to a later version of Deno. Thankies!`
+
+  if (isDenoOutdated) {
+    console.info(msg('Deno', currentDeno, expectedDeno));
+  }
+  if (isV8Outdated) {
+    console.info(msg('V8', currentV8, expectedV8))
+  }
+  if (isTypescriptOutdated) {
+    console.info(msg('Typescript', currentTypescript, expectedTypescript))
+  }
+
+  if (isDenoOutdated || isV8Outdated || isTypescriptOutdated) {
+    Deno.exit(1)
+  }
 }
--- src/compare.ts
+++ src/compare.ts
@@ -5,21 +5,19 @@ export function isOutdated(
   const minimumVersionArr = minimumVersion.split(".");
   const actualVersionArr = actualVersion.split(".");
 
-  // versionCategory includes 'major', 'minor', 'patch', if we are
-  // talking about semvar
-  versionCategoryEnumeration: for (let i = 0; i < minimumVersionArr.length; ++i) {
-    const minimumVersionCategoryNum = parseInt(minimumVersionArr[i])
-    const actualVersionCategoryNum = parseInt(actualVersionArr[i])
+  // versionCategory includes 'major', 'minor', 'patch', ex. if semvar
+  versionCategoryEnumeration:
+  for (let i = 0; i < minimumVersionArr.length; ++i) {
+    const minimumVersionCategoryNum = parseInt(minimumVersionArr[i]);
+    const actualVersionCategoryNum = parseInt(actualVersionArr[i]);
 
-    // if true, for any versionCategory, then whole version is out of date
-    // console.log(minimumVersion, `is ${minimumVersionCategoryNum} greater than ${actualVersionCategoryNum}?`, minimumVersionCategoryNum > actualVersionCategoryNum)
+    // if this is true for any versionCategory, then whole version is out of date
     if (minimumVersionCategoryNum > actualVersionCategoryNum) {
-      return true
+      return true;
     } else if (minimumVersionCategoryNum === actualVersionCategoryNum) {
-      continue versionCategoryEnumeration
-    }
-    else {
-      break versionCategoryEnumeration
+      continue versionCategoryEnumeration;
+    } else {
+      break versionCategoryEnumeration;
     }
   }
 
