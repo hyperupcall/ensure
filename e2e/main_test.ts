4b8931b0c47c9b62872c941df46c161f63de5610
eankeen <24364012+eankeen@users.noreply.github.com>
2020-05-16 01:09:25 -0700

    fix: actuall fix merge conflict

--- e2e/main_test.ts
+++ e2e/main_test.ts
@@ -1,35 +1,8 @@
 import * as path from "https://deno.land/std@v0.51.0/path/mod.ts";
 import { assertEquals } from "https://deno.land/std@v0.50.0/testing/asserts.ts";
 
-<<<<<<< HEAD
-const _dirname = path.dirname(new URL(import.meta.url).pathname);
-=======
 let _dirname = path.dirname(new URL(import.meta.url).pathname);
 if (Deno.build.os == "windows") _dirname = _dirname.slice(1)
-
-console.log("a", _dirname)
-
-if (Deno.build.os === "windows") {
-  Deno.test({
-  name: "exit code of 0 when running shouldPass",
-  fn: async (): Promise<void> => {
-    const scriptName = path.join(_dirname, "shouldPass.ts");
-    const proc = Deno.run({
-      stdout: "piped",
-      stderr: "inherit",
-      stdin: "inherit",
-      cmd: ["deno", "run", scriptName],
-    });
-
-    await proc.output();
-    const status = await proc.status();
-    proc.close();
-
-    assertEquals(status.code, 0);
-  },
-});
-}
->>>>>>> windows-squash
 
 Deno.test({
   name: 'exit code of 0 when running shouldPass',
